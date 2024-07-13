1e4652a3179875bef8f2d4c4cbb411261c2d2cc1
taiht <Hdt13102k3@gmail.com>
2024-07-13 19:36:48 +0700

    inventory

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+           'name' => 'required|max:255',
+           'description' => 'required',
+           'price' => 'required|numeric'
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json($product, 201);
+    }
+    public function show(Product $product)
+    {
+        if ($product) {
+            return response()->json($product, 200);
+        } else {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+    }
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric'
+        ]);
+
+        $product = Product::update($validated);
+
+        return response()->json($product, 200);
+    }
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json('Deleted', 200);
+    }
+}
